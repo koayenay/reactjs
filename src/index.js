d92787ac31e404156e58f587b4b5aca7fde541ca
koayenay <koayenay@gmail.com>
2023-06-21 10:32:18 -0700

    conditional and destructuring

--- src/index.js
+++ src/index.js
@@ -62,14 +62,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData
+  const numPizzas = pizzas.length
   return (
     <div>
       <h2>Our Menu</h2>
-      <ul className='pizzas'>
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className='pizzas'>
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later!</p>
+      )}
+
       {/* <Pizza
         name='Pizza Spinaci'
         ingredients='Tomato, mozarella, ham, aragula, and burrata cheese'
@@ -87,14 +94,15 @@ function Menu() {
   )
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null
   return (
     <li className='pizza'>
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   )
@@ -102,18 +110,35 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours()
-  const openHour = 12
+  const openHour = 10
   const closeHour = 22
 
   const isOpen = hour >= openHour && hour <= closeHour
 
   return (
     <footer className='footer'>
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {closeHour}:00 and {closeHour}:00
+        </p>
+      )}
     </footer>
   )
 }
-
+function Order({ closeHour, openHour }) {
+  return (
+    <div className='order'>
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online
+      </p>
+      {new Date().toLocaleTimeString()}. We're currently open
+      <button className='btn'>Order</button>
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
